91505589d0fe93af95447ec2ec2c24e0c77e3842
serhank <serhankars@hotmail.com>
2024-12-25 00:46:47 +0000

    Added neovim to installed packages

--- install-favorite-packages.sh
+++ install-favorite-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install exa tldr bat -y
+sudo apt-get install exa tldr bat hwinfo neovim -y
 tldr -u
 rm code_arm64.deb
 wget -O code_arm64.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64"
